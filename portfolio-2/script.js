112c31bd87821c33590ade387aa0f8e17c530bc2
Rahul Singh <137313478+azeebneuron@users.noreply.github.com>
2024-06-27 15:51:46 +0530

    here we go

---
+++ portfolio-2/script.js
@@ -0,0 +1,25 @@
+const parallaxImages = document.querySelectorAll(".parallax-image");
+const featuresEl = document.querySelector(".features");
+const featureEls = document.querySelectorAll(".feature");
+
+// parallax scroll javascript
+window.addEventListener("scroll", () => {
+  const scrollValue = window.scrollY;
+  parallaxImages.forEach((parallaxImage) => {
+    const parallaxSpeed = parallaxImage.dataset.speed;
+    const topPosition = -(scrollValue * parallaxSpeed) / 100;
+    parallaxImage.style.transform = `translateY(${topPosition}px) scale(1.2)`;
+  });
+});
+
+// skills glow javascript
+featuresEl.addEventListener("pointermove", (ev) => {
+  featureEls.forEach((featureEl) => {
+    // Not optimized yet, I know
+    const rect = featureEl.getBoundingClientRect();
+
+    featureEl.style.setProperty("--x", ev.clientX - rect.left);
+    featureEl.style.setProperty("--y", ev.clientY - rect.top);
+  });
+});
+
